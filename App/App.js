a76f022af4a4f20936093aaea08ebf155ebccbc3
hyunWoo <skadudgh1341@gmail.com>
2020-11-13 17:34:52 +0900

    ..

--- App/App.js
+++ App/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-
+import * as Location from './src/Location';
 const App = () => {
+
+  const [location, setLocation] = useState('');
+  Location.getLocation().then(result=>{
+      setLocation(result);
+  })
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
+      <Text>{location}</Text>
     </SafeAreaView>
   );
 };
