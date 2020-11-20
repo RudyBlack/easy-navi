88a2da06a53a12bc0576b752ed2d4e196af672a9
남현우 <skadudgh1341@gmail.com>
2020-11-20 07:21:41 +0000

    Set websocket

--- Web/frontend/src/js/main.js
+++ Web/frontend/src/js/main.js
@@ -1,17 +1,23 @@
 import { enviroment } from './utils/check/enviromentCheck.js';
 import { outputLog } from './utils/debug/console.js';
-import { receiveDataParse } from './utils/parse/dataParse.js';
+import { dataParse } from './utils/parse/dataParse.js';
 import { StateManagement } from './utils/dataManager/globalData.js';
 
 import './receive/receiveFromNative.js';
+import './receive/receiveFromSocketServer.js';
+
 import { sendToNative } from './send/sendToNative.js';
+import { sendToSocketServer } from './send/sendToSocketServer.js';
 
 import { kakaoMap } from './api/map/kakaoMap.js';
 
-//receive -> Native
 StateManagement.regObserver('receiveFromNative', (e) => {
-    let { altitude, latitude, longitude } = receiveDataParse(e.data);
+    let { altitude, latitude, longitude } = dataParse(e.data);
     StateManagement.set('altitude', altitude).set('latitude', latitude).set('longitude', longitude);
+});
+
+StateManagement.regObserver('receiveFromSocketServer', (e) => {
+    console.log(e);
 });
 
 kakaoMap(StateManagement);
@@ -27,3 +33,4 @@ if (enviroment === 'web') {
 if (enviroment === 'webview') {
     sendToNative('location');
 }
+
