46b19b6d19dae2b1eb9b66c645c97416d3786294
RudyBlack <skadudgh1341@gmail.com>
2020-11-27 17:11:25 +0900

    Set https, back/ -> backend/ path in package.json

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const io = require('socket.io')(http);
 const socketio = require('socket.io');
 
-const port = 3000;
+const port = 443;
+
+const serverOption = {
+  ca : fs.readFileSync('./.ssl/ca_bundle.crt'),
+  key: fs.readFileSync('./.ssl/private.key'),
+  cert: fs.readFileSync('./.ssl/certificate.crt'),
+};
 
 require('./socket/socket.js');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, './frontend', 'index.html'));
 });
 
 app.route('/src/*').all(function (req, res) {
-    res.sendFile(path.join(__dirname, '../frontend', req.url));
+    res.sendFile(path.join(__dirname, './frontend', req.url));
 });
 
-http.listen(3000, function () {
-    console.log('app is running on port: 3000');
+https.createServer(serverOption, app).listen(port, () => {
+  console.log('HTTPS Server Started');
 });
+
 
 io.on('connection', function (socket) {
     console.log('user connected: ', socket.id);
